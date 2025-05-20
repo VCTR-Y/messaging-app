6eb7ed3da466065a21e61b26a5d301db7ed8922f
victor <victorfengye2004@gmail.com>
2025-05-20 15:20:13 -0700

    add messaging route

---
+++ backend/src/controllers/message.controller.js
@@ -0,0 +1,71 @@
+import User from "../models/User.js";
+import Message from "../models/Message.js";
+import cloudinary from "../lib/cloudinary.js";
+
+export const  getUsersForSidebar = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+    const filteredUsers = await User.find({_id: { $ne: loggedInUserId }}).select("-password ");
+
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.error("Error in getUsersForSidebar: ", error.message);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id:userToChatId } = req.params;
+    const myId = req.user._id;
+
+    const messages = await Message.find({
+      $or: [
+        { sender: myId, receiver: userToChatId },
+        { sender: userToChatId, receiver: myId },
+      ],
+    })
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessages: ", error.message);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const {text, image } = req.body;
+    const {id: receiverId} = req.params;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    if (image) {
+      const uploadedImage = await cloudinary.uploader.upload(image);
+      imageUrl = uploadedImage.secure_url;
+    } 
+
+    const newMessage = await Message.create({
+      senderId: senderId,
+      receiverId: receiverId,
+      text: text,
+      image: imageUrl,
+    });
+
+    await newMessage.save();
+
+    // todo: realtime functonality goes here
+
+    res.status(201).json({newMessage});
+    
+  } catch (error) {
+    console.log("Error in sendMessage: ", error.message);
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
